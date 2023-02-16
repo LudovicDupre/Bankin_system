add97092f9b92fecb145f237fe8e073a4afc2408
Pastor Guillaume <guillaume.pastor@fms-ea.com>
2023-02-16 13:37:05 +0100

    condition operation

--- src/fr/bankSyst/Operation.java
+++ src/fr/bankSyst/Operation.java
@@ -2,33 +2,33 @@ package fr.bankSyst;
 
 public interface Operation {
 	
-/** transfer of an amount
- * @param idAccount withdrawal from this account
- * @param idAccount2 deposit from this account
- * @param amount moved
- * @param balance original balance
- * @return balance after operation
- */
-	static double transfer(String idAccount , String idAccount2, double amount, double balance) {
-		
-		return balance;
-	}
+
+
 	/** withdrawal operation
 	 * @param idAccount to withdraw from
 	 * @param balance 
 	 * @param amount
 	 * @return
 	 */
-	static double withdrawal(String idAccount, double balance, double amount) {
+	static double withdrawal(double balance, double amount, double overdraft) {
 		
-		return balance-amount;
+		if(balance-amount < overdraft) {
+			System.out.println("You cannot withdraw cause your overdraft is riched");
+			
+			return balance;
+		}else {
+			return balance-amount;
+		}
+		
 	}
 	/** deposit operation
 	 * @param x ID du compte
 	 * @param y solde avant ajout 
 	 * @return balance after operation
 	 */
-	static double deposit(String idCompte, double balance, double amount) {
+	static double deposit(int idCompte, double balance, double amount) {
+
+		
 		
 		return balance+amount;
 	}
